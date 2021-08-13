8a5ba06259122d15fa5b69e7a2beeeece66a122a
mwc34 <mwc34@cam.ac.uk>
2021-08-13 12:32:53 +0100

    al-hadikhia

--- client/json/sync_info.py
+++ client/json/sync_info.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 import requests
 
 path = os.path.dirname(os.path.abspath(__file__))
@@ -9,9 +10,10 @@ roles = json.load(open(os.path.join(path, 'roles.json'), encoding='utf-8'))
 ref = {r['id'] : r for r in requests.get("https://raw.githubusercontent.com/bra1n/townsquare/main/src/roles.json").json()}
 
 for r in roles:
-    if r['id'] in ref:
-        r['firstNight'] = ref[r['id']]['firstNight']
-        r['otherNight'] = ref[r['id']]['otherNight']
-        r['ability'] = ref[r['id']]['ability']
+    mutated_id = re.sub(r'-_', '', r['id'])
+    if mutated_id in ref:
+        r['firstNight'] = ref[mutated_id]['firstNight']
+        r['otherNight'] = ref[mutated_id]['otherNight']
+        r['ability'] = ref[mutated_id]['ability']
 
 json.dump(roles, open(os.path.join(path, 'roles.json'), 'w+', encoding='utf-8'), indent=4, separators=(',',':'))
