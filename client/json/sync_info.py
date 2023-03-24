e2b58cf321d5d443664e2ec8bf20df1af722f245
Malcolm <mwchadwick@outlook.com>
2023-03-24 12:16:22 +0000

    sync roles.json

--- client/json/sync_info.py
+++ client/json/sync_info.py
@@ -10,7 +10,7 @@ roles = json.load(open(os.path.join(path, 'roles.json'), encoding='utf-8'))
 ref = {r['id'] : r for r in requests.get("https://raw.githubusercontent.com/bra1n/townsquare/develop/src/roles.json").json()}
 
 for r in roles:
-    mutated_id = re.sub(r'-_', '', r['id'])
+    mutated_id = re.sub(r'-|_', '', r['id'])
     if mutated_id in ref:
         r['firstNight'] = ref[mutated_id]['firstNight']
         r['otherNight'] = ref[mutated_id]['otherNight']
